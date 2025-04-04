692d0953fded6fe002b822d8dd7128cefb52b1c6
Zavalny Aleksandr <aleksandr.s.zavalny@raiffeisen.ru>
2025-04-04 16:19:53 +0600

    add api

--- modules/api/build.gradle.kts
+++ modules/api/build.gradle.kts
@@ -1,3 +1,12 @@
 dependencies {
     api(project(":core"))
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("javax.validation:validation-api:2.0.0.Final")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
 }
